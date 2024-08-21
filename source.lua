d13f5a7183e30c9ce883b342ebda40e6010fb504
dennis7777777 <167103475+dennis7777777@users.noreply.github.com>
2024-08-22 00:56:04 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -875,7 +875,7 @@ end
 function RayfieldLibrary:CreateWindow(Settings)
 	local Passthrough = false
 	Topbar.Title.Text = Settings.Name
-	Main.Size = UDim2.new(0, 240, 0, 420)
+	Main.Size = UDim2.new(0, 0.5, 0, 0.5)
 	Main.Visible = false
 	Main.BackgroundTransparency = 1
 	LoadingFrame.Title.TextTransparency = 1
